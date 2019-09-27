93bc1229edd8c9fadc60540eaeda06d046a03b95
xmjr <xinlianglai@163.com>
2019-09-27 15:10:08 +0800

    deploy

---
+++ precache-manifest.c1449b73dfae4d528b0d7ff62679626e.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = [
+  {
+    "revision": "4a6af6eada0302dea66d",
+    "url": "/travels/css/app.93c49190.css"
+  },
+  {
+    "revision": "4a6af6eada0302dea66d",
+    "url": "/travels/js/app.80c3d4f5.js"
+  },
+  {
+    "revision": "2e449140360084a391ca",
+    "url": "/travels/css/chunk-29452980.757f35ae.css"
+  },
+  {
+    "revision": "2e449140360084a391ca",
+    "url": "/travels/js/chunk-29452980.ff9a9930.js"
+  },
+  {
+    "revision": "2176e40a805ed20fa918",
+    "url": "/travels/js/chunk-4ffdd95a.48bb61af.js"
+  },
+  {
+    "revision": "6a68f72224d5dc3a7b98",
+    "url": "/travels/css/chunk-762ceb37.e9f747c0.css"
+  },
+  {
+    "revision": "6a68f72224d5dc3a7b98",
+    "url": "/travels/js/chunk-762ceb37.703c3c7f.js"
+  },
+  {
+    "revision": "679b7010b986633104c6",
+    "url": "/travels/css/chunk-bafac352.be50fd70.css"
+  },
+  {
+    "revision": "679b7010b986633104c6",
+    "url": "/travels/js/chunk-bafac352.de7ee463.js"
+  },
+  {
+    "revision": "d98a624c2e3490e73085",
+    "url": "/travels/css/chunk-vendors.c412edf5.css"
+  },
+  {
+    "revision": "d98a624c2e3490e73085",
+    "url": "/travels/js/chunk-vendors.863720b3.js"
+  },
+  {
+    "revision": "5828168b0bcd3f3684b1660c167b7f58",
+    "url": "/travels/img/iconfont.5828168b.svg"
+  },
+  {
+    "revision": "395de6bd5bf52cd7ec37c9dd73d2116c",
+    "url": "/travels/index.html"
+  },
+  {
+    "revision": "96a7caea90aae8a467d288ccf3cbe402",
+    "url": "/travels/api/index.json"
+  },
+  {
+    "revision": "2d631f7d51b3f406b3ac764ec3e361f0",
+    "url": "/travels/api/detail.json"
+  },
+  {
+    "revision": "a86c2684caa29fcf9dcda18791293b6d",
+    "url": "/travels/api/city.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/travels/robots.txt"
+  }
+];
